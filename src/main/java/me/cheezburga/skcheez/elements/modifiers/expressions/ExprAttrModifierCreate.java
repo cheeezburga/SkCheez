fc3558f68353d3cfe96581aeb26cfdd18104d687
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-11 02:36:02 +1000

    Made operation optional in pattern, and changes to toString(). Added logic later to handle this, with a default of ADD_NUMBER.
    Spent a while wondering if this will ever lead to problems further down the line, but I don't think it will. Check this more later. The problem I was so concerned with was that the created wrapper would be incomplete (either the attribute or modifier), and so when trying to get its properties/add or remove it to an item there might be some problems.
    Is this even something to worry about though? Aren't all wrappers created via this expression complete (with default values if some expressions in pattern are not provided)?

--- src/main/java/me/cheezburga/skcheez/elements/modifiers/expressions/ExprAttrModifierCreate.java
+++ src/main/java/me/cheezburga/skcheez/elements/modifiers/expressions/ExprAttrModifierCreate.java
@@ -20,7 +20,7 @@ public class ExprAttrModifierCreate extends SimpleExpression<AttributeModifierWr
 
     static {
         Skript.registerExpression(ExprAttrModifierCreate.class, AttributeModifierWrapper.class, ExpressionType.COMBINED,
-                "[new] %attributetype% [attribute] modifier (named|with name|with id) %string% with amount %number% with operation %attributemodifieroperation% " +
+                "[new] %attributetype% [attribute] modifier (named|with name|with id) %string% with amount %number% [with operation %attributemodifieroperation%] " +
                 "[with uuid %-string%] [for [equipment] slot %-equipmentslot%]");
     }
 
@@ -46,25 +46,31 @@ public class ExprAttrModifierCreate extends SimpleExpression<AttributeModifierWr
     @SuppressWarnings("NullableProblems")
     @Override
     protected @Nullable AttributeModifierWrapper[] get(Event event) {
-        if (this.type == null || this.name == null || this.amount == null || this.operation == null) return null;
+        if (this.type == null || this.name == null || this.amount == null) return null;
 
         Attribute type = this.type.getSingle(event);
         String name = this.name.getSingle(event);
         Number amount = this.amount.getSingle(event);
-        AttributeModifier.Operation operation = this.operation.getSingle(event);
-        if (type == null || name == null || amount == null || operation == null) return null;
+        if (type == null || name == null || amount == null) return null;
 
         AttributeModifier toWrap;
+
+        AttributeModifier.Operation operation = AttributeModifier.Operation.ADD_NUMBER;
+        if (this.operation != null) {
+            AttributeModifier.Operation o = this.operation.getSingle(event);
+            if (o != null) { operation = o; }
+        }
+
+        EquipmentSlot slot = null;
+        if (this.slot != null) {
+            EquipmentSlot s = this.slot.getSingle(event);
+            if (s != null) { slot = s; }
+        }
 
         UUID uuid = UUID.randomUUID();
         if (this.uuid != null) {
             String u = this.uuid.getSingle(event);
             if (u != null) { uuid = UUID.fromString(u); }
-        }
-        EquipmentSlot slot = null;
-        if (this.slot != null) {
-            EquipmentSlot s = this.slot.getSingle(event);
-            if (s != null) { slot = s; }
         }
 
         if (slot == null) {
@@ -87,11 +93,12 @@ public class ExprAttrModifierCreate extends SimpleExpression<AttributeModifierWr
 
     @Override
     public @NotNull String toString(@Nullable Event event, boolean b) {
-        String name = "attribute modifier with name " + this.name.toString(event, b);
+        String type = this.type.toString(event, b);
+        String name = " attribute modifier with name " + this.name.toString(event, b);
         String amount = " with amount " + this.amount.toString(event, b);
-        String operation = " with operation " + this.operation.toString(event, b);
+        String operation = this.operation != null ? " with operation " + this.operation.toString(event, b) : "";
         String uuid = this.uuid != null ? " with uuid " + this.uuid.toString(event, b) : "";
         String slot = this.slot != null ? " with slot " + this.slot.toString(event, b) : "";
-        return name + amount + operation + uuid + slot;
+        return type + name + amount + operation + uuid + slot;
     }
 }
