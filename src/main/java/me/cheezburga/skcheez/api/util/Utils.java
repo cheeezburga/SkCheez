5665ee157052cc58c860eb57f316ad81cff7b86d
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-12 00:34:57 +1000

    Added javadocs.

--- src/main/java/me/cheezburga/skcheez/api/util/Utils.java
+++ src/main/java/me/cheezburga/skcheez/api/util/Utils.java
@@ -3,6 +3,13 @@ package me.cheezburga.skcheez.api.util;
 import java.util.UUID;
 
 public class Utils {
+
+    /**
+     * Converts a {@link UUID UUID} to an array of integers.
+     *
+     * @param uuid The UUID to convert.
+     * @return An array of integers representing the UUID.
+     */
     public static int[] uuidToIntArray(UUID uuid) {
         long most = uuid.getMostSignificantBits();
         long least = uuid.getLeastSignificantBits();
@@ -14,10 +21,22 @@ public class Utils {
         };
     }
 
+    /**
+     * Converts an array of integers representing a {@link UUID uuid} to a string.
+     *
+     * @param array The array of integers representing the UUID.
+     * @return A string representation of the UUID array.
+     */
     public static String uuidIntArrayToString(int[] array) {
         return String.format("[I:%d,%d,%d,%d]", array[0], array[1], array[2], array[3]);
     }
 
+    /**
+     * Converts a string representation of a UUID array to an array of integers.
+     *
+     * @param uuid The string representation of the UUID array.
+     * @return An array of integers representing the UUID.
+     */
     public static int[] uuidStringToIntArray(String uuid) {
         String[] components = uuid.replace("[I;", "").replace("]", "").split(",");
         return new int[]{
@@ -28,6 +47,12 @@ public class Utils {
         };
     }
 
+    /**
+     * Converts an array of integers representing a {@link UUID UUID} back to a UUID.
+     *
+     * @param array The array of integers representing the UUID.
+     * @return The UUID.
+     */
     public static UUID uuidIntArrayToUUID(int[] array) {
         long most = (((long) array[0]) << 32) | (array[1] & 0xFFFFFFFFL);
         long least = (((long) array[2]) << 32) | (array[3] & 0xFFFFFFFFL);
