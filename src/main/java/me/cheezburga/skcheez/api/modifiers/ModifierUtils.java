718f3a0710398a3e6e887643f204dbac65135d6e
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-12 00:43:59 +1000

    Added javadocs.

--- src/main/java/me/cheezburga/skcheez/api/modifiers/ModifierUtils.java
+++ src/main/java/me/cheezburga/skcheez/api/modifiers/ModifierUtils.java
@@ -11,7 +11,17 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
+/**
+ * Utility methods for {@link Attribute attributes}, {@link AttributeModifier modifiers}, and their {@link AttributeModifierWrapper wrappers}.
+ */
 public class ModifierUtils {
+
+    /**
+     * Converts an array of {@link AttributeModifierWrapper wrappers} to a {@link Multimap multimap} of attributes to modifiers.
+     *
+     * @param wrappers The wrappers to convert.
+     * @return A Multimap containing attributes mapped to modifiers.
+     */
     public static Multimap<Attribute, AttributeModifier> convertWrappers(AttributeModifierWrapper[] wrappers) {
         Multimap<Attribute, AttributeModifier> map = HashMultimap.create();
         for (AttributeModifierWrapper wrapper : wrappers) {
@@ -20,9 +30,17 @@ public class ModifierUtils {
         return map;
     }
 
+    /**
+     * Gets a conflicting modifier for the given {@link Attribute attribute} and {@link AttributeModifier modifier} from the given {@link ItemMeta ItemMeta}.
+     *
+     * @param meta The item meta to check for conflicting modifiers.
+     * @param attribute The attribute to check with.
+     * @param modifier The modifier to check with.
+     * @return The conflicting modifier, or null if no conflict is found.
+     */
     public static @Nullable AttributeModifier getConflictingModifier(ItemMeta meta, Attribute attribute, AttributeModifier modifier) {
         // this initially returned a wrapper, not just the modifier
-        // if there can ever be a conflicting modifier NOT of the same attribute, change back
+        // if there can ever be a conflicting modifier NOT of the same attribute, change back and adjust logic in modifiers of item expression
         if (!meta.hasAttributeModifiers()) { return null; }
         else {
             Collection<AttributeModifier> modifiers = meta.getAttributeModifiers(attribute);
@@ -40,6 +58,12 @@ public class ModifierUtils {
         return null;
     }
 
+    /**
+     * Gets the NBT string representation of an equipment slot.
+     *
+     * @param slot The equipment slot.
+     * @return The NBT string representation of the slot.
+     */
     public static String getSlotNBTString(EquipmentSlot slot) {
         return switch (slot) {
             case HAND -> "mainhand";
