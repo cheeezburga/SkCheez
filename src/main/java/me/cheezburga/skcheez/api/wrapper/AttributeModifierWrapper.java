6e4266af92d5de29141d5b704d6d5caf802aada3
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-11 16:00:27 +1000

    Implemented all cases for toString() method. Removed unnecessary imports.

--- src/main/java/me/cheezburga/skcheez/api/wrapper/AttributeModifierWrapper.java
+++ src/main/java/me/cheezburga/skcheez/api/wrapper/AttributeModifierWrapper.java
@@ -7,8 +7,6 @@ import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.inventory.EquipmentSlot;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringJoiner;
 import java.util.UUID;
 
@@ -84,16 +82,27 @@ public class AttributeModifierWrapper {
         }
     }
 
-    @SuppressWarnings("ConstantValue")
     public String toString() {
-        String type = (attribute != null) ? attribute.toString().toLowerCase().replace("_", " ") : "";
-        String name = (modifier != null) ? "\"" + modifier.getName() + "\"" : "";
-        String amount = (modifier != null) ? String.valueOf(modifier.getAmount()) : "";
-        String operation = (modifier != null) ? modifier.getOperation().toString().toLowerCase().replace("_", " ") : "";
-        String slot = (modifier != null && modifier.getSlot() != null) ? modifier.getSlot().toString().toLowerCase() : "any";
-        String uuid = (modifier != null) ? modifier.getUniqueId().toString() : "";
-
-        return String.format("%s attribute modifier with name %s, amount %s, operation %s, slot %s, and uuid %s",
-                type, name, amount, operation, slot, uuid);
+        if (attribute != null && modifier != null) {
+            String type = attribute.toString().toLowerCase().replace("_", " ");
+            String name = "\"" + modifier.getName() + "\"";
+            String amount = String.valueOf(modifier.getAmount());
+            String operation = modifier.getOperation().toString().toLowerCase().replace("_", " ");
+            String slot = modifier.getSlot() != null ? modifier.getSlot().toString().toLowerCase() : "any";
+            String uuid = Utils.uuidIntArrayToString(Utils.uuidToIntArray(modifier.getUniqueId()));
+            return String.format("%s attribute modifier with name %s, amount %s, operation %s, slot %s, and uuid %s",
+                    type, name, amount, operation, slot, uuid);
+        } else if (attribute != null) {
+            return "empty " + attribute.toString().toLowerCase().replace("_", " ") + " attribute modifier";
+        } else if (modifier != null) {
+            String name = "\"" + modifier.getName() + "\"";
+            String amount = String.valueOf(modifier.getAmount());
+            String operation = modifier.getOperation().toString().toLowerCase().replace("_", " ");
+            String slot = modifier.getSlot() != null ? modifier.getSlot().toString().toLowerCase() : "any";
+            String uuid = modifier.getUniqueId().toString();
+            return String.format("%s attribute modifier with name %s, amount %s, operation %s, slot %s, and uuid %s",
+                    "unknown", name, amount, operation, slot, uuid);
+        }
+        return null;
     }
 }
