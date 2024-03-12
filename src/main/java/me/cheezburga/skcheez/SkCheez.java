481599d7eda1e5531c9c926a1af6bae02501d987
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-03-12 18:32:54 +1000

    Updated. - Should now use update checker. - Cleaned up class in general. - onEnable() method now implements update checker stuff and reads nicer.

--- src/main/java/me/cheezburga/skcheez/SkCheez.java
+++ src/main/java/me/cheezburga/skcheez/SkCheez.java
@@ -2,6 +2,11 @@ package me.cheezburga.skcheez;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
+import me.cheezburga.skcheez.api.util.UpdateChecker;
+import me.cheezburga.skcheez.api.util.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -10,23 +15,37 @@ public final class SkCheez extends JavaPlugin {
 
     private static SkCheez instance;
     private SkriptAddon addon;
+    private PluginManager manager;
+
+    @SuppressWarnings("unused")
+    public static SkCheez getPlugin() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        if (!Skript.isRunningMinecraft(1,17)) {
-            getPluginLoader().disablePlugin(this);
-            getLogger().info("SkCheez only works on 1.17+");
-            return;
-        }
+        long start = System.currentTimeMillis();
         instance = this;
-        loadMetrics();
+        manager = Bukkit.getPluginManager();
+
         try {
-            this.addon = Skript.registerAddon(this)
+            addon = Skript.registerAddon(this)
                     .loadClasses("me.cheezburga.skcheez", "elements")
                     .setLanguageFileDirectory("lang");
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load SkCheez.", e);
+            throw new RuntimeException("Failed to load SkCheez!", e);
         }
+
+        loadMetrics();
+
+        String version = getDescription().getVersion();
+        checkUpdate(version);
+
+        Utils.log("&aEnabled v%s&7 in &b%.2f seconds", version, (float) (System.currentTimeMillis() - start) / 1000);
+    }
+
+    private void checkUpdate(String version) {
+        UpdateChecker.checkForUpdate(version);
     }
 
     private void loadMetrics() {
@@ -34,10 +53,12 @@ public final class SkCheez extends JavaPlugin {
         metrics.addCustomChart(new Metrics.SimplePie("skriptVersion", () -> Skript.getVersion().toString()));
     }
 
-    public static SkCheez getInstance() {
-        return instance;
+    @SuppressWarnings("unused")
+    public void registerListener(Listener listener) {
+        manager.registerEvents(listener, instance);
     }
 
+    @SuppressWarnings("unused")
     public SkriptAddon getAddonInstance() {
         return addon;
     }
